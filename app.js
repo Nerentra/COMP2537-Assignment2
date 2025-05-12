a679c8274f0162041aff4bde7aae4a12dbc687e5
Nerentra <60801196+Nerentra@users.noreply.github.com>
2025-05-12 14:50:50 -0700

    Implement admin page

--- app.js
+++ app.js
@@ -46,15 +46,36 @@ const sessionExpireTime = 1000 * 60 * 60; // 1 hour
 
 const mongoStore = MongoStore.create({
     mongoUrl: mongodbUri,
-    collectionName: "assignment1sessions",
+    collectionName: "1537sessions",
     crypto: {
         secret: mongodbSessionSecret,
     },
 });
 
-function loginUser(req, name) {
+function loginUser(req, user) {
     req.session.authenticated = true;
-    req.session.name = name;
+    req.session.name = user.name;
+    req.session.admin = user.admin ?? false;
+}
+
+function authenticatedMiddleware(req, res, next) {
+    if (req.session.authenticated === true) {
+        next();
+    }
+    else {
+        res.redirect("/");
+    }
+}
+
+function adminMiddleware(req, res, next) {
+    if (req.session.admin === true) {
+        next();
+    } else if (req.session.authenticated === true) {
+        res.redirect("/members");
+    }
+    else {
+        res.status(403).render("403");
+    }
 }
 
 app.set("view engine", "ejs");
@@ -85,7 +106,7 @@ app.post("/signupSubmit", async (req, res) => {
     const { name, email, password } = req.body;
     const schema = Joi.object({
         name: Joi.string().alphanum().max(20).required(),
-        email:    Joi.string().email().max(30).required(),
+        email: Joi.string().email().max(30).required(),
         password: Joi.string().max(20).required()
     });
     const validationResult = schema.validate({
@@ -97,7 +118,7 @@ app.post("/signupSubmit", async (req, res) => {
         res.render("signupSubmit", { errorText: validationResult.error });
         return;
     }
-    const user = await mongodbDatabase.collection("assignment1users").findOne({
+    const user = await mongodbDatabase.collection("1537users").findOne({
         email,
     });
     if (user !== null) {
@@ -106,14 +127,15 @@ app.post("/signupSubmit", async (req, res) => {
     }
     const passwordHash = await bcrypt.hash(req.body.password, saltRounds);
     mongodbDatabase
-        .collection("assignment1users")
+        .collection("1537users")
         .insertOne({
             name: req.body.name,
             email: req.body.email,
+            admin: false,
             passwordHash,
         })
         .then(() => {
-            loginUser(req, name);
+            loginUser(req, { name });
             res.redirect("/members");
         })
         .catch((err) => {
@@ -140,7 +162,7 @@ app.post("/loginSubmit", async (req, res) => {
         res.render("loginSubmit", { errorText: validationResult.error });
         return;
     }
-    const user = await mongodbDatabase.collection("assignment1users").findOne({
+    const user = await mongodbDatabase.collection("1537users").findOne({
         email,
     });
     if (user === null) {
@@ -152,25 +174,51 @@ app.post("/loginSubmit", async (req, res) => {
         res.render("loginSubmit", { errorText: "Incorrect password." });
         return;
     }
-    loginUser(req, user.name);
+    loginUser(req, user);
     res.redirect("/members");
 })
 
-app.get("/members", (req, res) => {
-    if(req.session.authenticated !== true) {
-        res.redirect("/");
-    } else {
-        const catIndex = Math.floor(Math.random() * cats.length);
-        res.render("members", {
-            name: req.session.name,
-            imgLink: cats[catIndex],
-        });
-    }
+app.get("/members", authenticatedMiddleware, (req, res) => {
+    const catIndex = Math.floor(Math.random() * cats.length);
+    res.render("members", {
+        name: req.session.name,
+        imgLink: cats[catIndex],
+    });
 })
 
 app.get("/logout", (req, res) => {
     req.session.destroy();
     res.redirect("/");
+})
+
+app.get("/admin", adminMiddleware, async (req, res) => {
+    const users = await mongodbDatabase
+        .collection("1537users")
+        .find()
+        .toArray();
+    res.render("admin", { users });
+})
+
+app.post("/admin/promoteUser/:email", adminMiddleware, async (req, res) => {
+    mongodbDatabase.collection("1537users").updateOne({
+        email: req.params.email,
+    }, {
+        $set: {
+            admin: true,
+        }
+    });
+    res.redirect("/admin");
+})
+
+app.post("/admin/demoteUser/:email", adminMiddleware, async (req, res) => {
+    mongodbDatabase.collection("1537users").updateOne({
+        email: req.params.email,
+    }, {
+        $set: {
+            admin: false,
+        }
+    });
+    res.redirect("/admin");
 })
 
 // Serve a 404 page for any other routes
